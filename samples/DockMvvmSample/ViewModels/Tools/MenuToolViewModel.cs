5e7c02e59eeb5e0f21b433281182a0e082a67a73
Dirk Lellinger <dlellinger@users.sourceforge.net>
2024-06-15 23:14:31 +0200

    Extend DockMvvmSample by means to create a new tool window from the main menu

---
+++ samples/DockMvvmSample/ViewModels/Tools/MenuToolViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Dock.Model.Mvvm.Controls;
+
+namespace DockMvvmSample.ViewModels.Tools;
+
+public class MenuToolViewModel : Tool
+{
+    public MenuToolViewModel()
+    {
+        Id = "Id of MenuTool";
+        Title = "MenuTool";
+        Context = "Context of MenuTool";
+    }
+}
